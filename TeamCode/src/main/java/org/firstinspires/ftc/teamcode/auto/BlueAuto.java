ca553a54e511146fea0f448b6f6497abde7554ae
DucksterQuackington <30yanga@gmail.com>
2026-09-16 16:17:01 -0500

    Added poses to Dev-bang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueAuto.java
@@ -5,12 +5,12 @@ import com.bylazar.configurables.annotations.Configurable;
 
 @Configurable
 public abstract class BlueAuto extends BozoAuto {
-    public static Pose startPose = new Pose(12, 72, Math.toRadians(90));
-    public static Pose shootLeftPose = new Pose(60, 108, Math.toRadians(90));
-    public static Pose shootRightPose = new Pose(60, 36, Math.toRadians(90));
-    public static Pose flowerLeftPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose flowerRightPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose endPose = new Pose(0, 0, Math.toRadians(90));
+    public static Pose startPose = new Pose(144 - 12, 144 - 72, Math.toRadians(270));
+    public static Pose shootLeftPose = new Pose(144 - 60, 144 - 108, Math.toRadians(270));
+    public static Pose shootRightPose = new Pose(144 - 60, 144 - 36, Math.toRadians(0));
+    public static Pose flowerLeftPose = new Pose(144 - 48, 144 - 132, Math.toRadians(0));
+    public static Pose flowerRightPose = new Pose(144 - 12, 144 - 48, Math.toRadians(180));
+    public static Pose endPose = new Pose(144 - 12, 144 - 72, Math.toRadians(0));
 
     @Override
     protected AutoConfig buildConfig() {
@@ -24,4 +24,4 @@ public abstract class BlueAuto extends BozoAuto {
         );
     }
 }
-}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedAuto.java
@@ -5,12 +5,12 @@ import com.bylazar.configurables.annotations.Configurable;
 
 @Configurable
 public abstract class RedAuto extends BozoAuto {
-    public static Pose startPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose shootLeftPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose shootRightPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose flowerLeftPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose flowerRightPose = new Pose(0, 0, Math.toRadians(90));
-    public static Pose endPose = new Pose(0, 0, Math.toRadians(90));
+    public static Pose startPose = new Pose(12, 72, Math.toRadians(270));
+    public static Pose shootLeftPose = new Pose(60, 108, Math.toRadians(270));
+    public static Pose shootRightPose = new Pose(60, 36, Math.toRadians(0));
+    public static Pose flowerLeftPose = new Pose(48, 132, Math.toRadians(0));
+    public static Pose flowerRightPose = new Pose(12, 48, Math.toRadians(180));
+    public static Pose endPose = new Pose(12, 72, Math.toRadians(0));
 
     @Override
     protected AutoConfig buildConfig() {
